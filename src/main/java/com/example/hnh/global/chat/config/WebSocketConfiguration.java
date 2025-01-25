9d8f0d557ba428560506567295c4601d85d9b2e3
searchcolor <qkrdydwo1022@gmail.com>
2025-01-25 15:57:55 +0900

    fix : WebSocketHandler 수정

--- src/main/java/com/example/hnh/global/chat/config/WebSocketConfiguration.java
+++ src/main/java/com/example/hnh/global/chat/config/WebSocketConfiguration.java
@@ -20,18 +20,8 @@ public class WebSocketConfiguration implements WebSocketConfigurer {
 
         registry
                 .addHandler(webSocketHandler,"/ws/chat")
-                .setAllowedOrigins("*")
-                .withSockJS()
-                .setHeartbeatTime(30);
+                .setAllowedOrigins("*");
 
-    }
-    @Bean
-    public ServletServerContainerFactoryBean createWebSocketContainer(){
-        // WebSocket의 런타임 특성 제어
-        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
-        container.setMaxTextMessageBufferSize(8192);
-        container.setMaxBinaryMessageBufferSize(8192);
-        return container;
     }
 
 //    @Bean

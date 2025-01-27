a1685d29ba879c328b547399eb2beee6779bbba8
SearchColor <152576373+SearchColor@users.noreply.github.com>
2025-01-27 13:51:21 +0900

    feat : interceptor config 추가

--- src/main/java/com/example/hnh/global/config/WebConfig.java
+++ src/main/java/com/example/hnh/global/config/WebConfig.java
@@ -1,120 +1,22 @@
 package com.example.hnh.global.config;
 
-import jakarta.servlet.DispatcherType;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.context.annotation.Bean;
+import com.example.hnh.global.config.interceptor.MemberRoleInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebSecurity // SecurityFilterChain 빈 설정을 위해 필요.
-@RequiredArgsConstructor
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    /**
-     * JWT 인증 Filter.
-     */
-    private final JwtAuthFilter jwtAuthFilter;
+    private final MemberRoleInterceptor memberRoleInterceptor;
 
-    /**
-     * AuthenticationProvider.
-     */
-    private final AuthenticationProvider authenticationProvider;
-
-    /**
-     * AuthenticationEntryPoint.
-     */
-//    private final AuthenticationEntryPoint authEntryPoint;
-    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
-
-    /**
-     * AccessDeniedHandler.
-     */
-//    private final AccessDeniedHandler accessDeniedHandler;
-    private final CustomAccessDeniedHandler customAccessDeniedHandler;
-
-    /**
-     * 화이트 리스트.
-     */
-    private static final String[] WHITE_LIST = {"/users/login", "/users/signup", "/error", "/admins/signup"
-            , "/ws/chat" , "/room" , "/chats/**"};
-
-    /**
-     * security 필터.
-     *
-     * @param http {@link HttpSecurity}
-     * @return {@link SecurityFilterChain} 필터 체인
-     */
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth ->
-                        auth.requestMatchers(WHITE_LIST).permitAll()
-                                // static 리소스 경로
-                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                                // 일부 dispatch 타입
-                                .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.INCLUDE,
-                                        DispatcherType.ERROR).permitAll()
-                                // path 별로 접근이 가능한 권한 설정
-                                        .requestMatchers("/admins/**").hasRole("ADMIN")
-                                // 나머지는 인증이 필요
-                                .anyRequest().authenticated()
-
-                )
-                // Spring Security 예외에 대한 처리를 핸들러에 위임.
-                .exceptionHandling(handler -> handler
-                        .authenticationEntryPoint(customAuthenticationEntryPoint)
-                        .accessDeniedHandler(customAccessDeniedHandler))
-                // JWT 기반 테스트를 위해 SecurityContext를 가져올 때 HttpSession을 사용하지 않도록 설정.
-                .sessionManagement(
-                        session
-                                -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
+    public WebConfig(MemberRoleInterceptor memberRoleInterceptor) {
+        this.memberRoleInterceptor = memberRoleInterceptor;
     }
 
-    /**
-     * 사용자 권한의 계층을 설정.
-     *
-     * @return {@link RoleHierarchy}
-     */
-    @Bean
-    public RoleHierarchy roleHierarchy() {
-        return RoleHierarchyImpl.fromHierarchy(
-                // "ROLE_ADMIN > ROLE_USER"
-                """
-                    ROLE_ADMIN > ROLE_USER
-                    """);
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 멤버 권한 인터셉터
+        registry.addInterceptor(memberRoleInterceptor);
     }
-
-    /**
-     * h2-console 접속은 Spring Security를 거치지 않도록 설정.
-     *
-     * @return {@link WebSecurityCustomizer}
-     * @see <a
-     * href="https://dukcode.github.io/spring/h2-console-with-spring-security/">spring-security에서-h2-console-사용하기</a>
-     */
-    @Bean
-    @ConditionalOnProperty(name = "spring.h2.console.enabled", havingValue = "true")
-    public WebSecurityCustomizer configureH2ConsoleEnable() {
-        return web -> web.ignoring().requestMatchers("/h2-console/**");
-    }
-
 }

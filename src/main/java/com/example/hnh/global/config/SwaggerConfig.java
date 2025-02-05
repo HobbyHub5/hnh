a02c0c9db563581d5b280792b07ba73c97771e47
박재혁 <jaeh3197@naver.com>
2025-02-05 15:58:49 +0900

    feat: 스웨거 연동 (#76)
    * feat: 스웨거 연동
    
    스웨거를 사용할 수 있도록 구현하였습니다.

---
+++ src/main/java/com/example/hnh/global/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.example.hnh.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("hobby hub")
+                        .version("v1.0")
+                        .description("소모임 프로젝트 API"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
